dd0b10ad9146981322e3aca42b1f794233a50c5f
Shapeotron <davidoleary1@yahoo.co.uk>
2015-11-16 15:49:19 +0000

    Two pickers, artist and track, with tracks selecting and playing

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 import AVFoundation
 
+class PopulationOfArrays {
+    
+    
+    func artistValues() -> [String] {
+        return ["ACDC", "BMTH", "Krush", "Tupac", "Notorious", "Shadow"]
+    }
+    
+    func trackValues(done: String) -> [String] {
+        if done == "ACDC" {
+            return ["Thunderstruck", "Big Gun", "Moneytalks", "Back In Black"]
+        }
+        if done == "BMTH" {
+            return ["Happy Song", "Throne", "True Friends", "Oh No"]
+        }
+        if done == "Krush" {
+            return ["Song 1", "Zen Approach", "Danger Of Love", "Candle Chant"]
+        }
+        if done == "Tupac" {
+            return ["Hit Em Up", "Changes", "All About You", "Dear Mama"]
+        }
+        if done == "Notorious" {
+            return ["Hypnotize", "Notorious Thugs", "Sky's The Limit", "You're Nobody"]
+        }
+        else {
+            return["Midnight", "Bloodstain", "Lonely Soul", "Unkle"]
+        }
+    }
+}
+
+
+
+
 class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var trackPicker: UIPickerView!
-    
+    @IBOutlet weak var artistPicker: UIPickerView!
     
     var audioPlayer = AVAudioPlayer()
     var isPlaying = false
@@ -24,6 +56,9 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
     var artistArray: [[String]] = [[String]]()
     var trackDictionary = [String: String]()
     var trackArray: [[String]] = [[String]]()
+    var picker1Options:[String] = []
+    var picker2Options:[String] = []
+
     
     @IBAction func playOrPauseMusic(sender: AnyObject) {
         if isPlaying {
@@ -63,9 +98,15 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         self.trackPicker.delegate = self
         self.trackPicker.dataSource = self
         
-        artistDictionary = ["ACDC": "ACDC", "BMTH": "BMTH", "Caribou":"Caribou", "David Guetta":"David Guetta", "Disclosure":"Disclosure", "Jon Hopkins":"Jon Hopkins", "DJ Krush":"DJ Krush","DJ Shadow":"DJ Shadow", "Tupac":"Tupac", "Notorious":"Notorious"]
-        trackDictionary = ["ACDC": "ACDC", "BMTH": "BMTH", "Caribou":"Caribou", "David Guetta":"David Guetta", "Disclosure":"Disclosure", "Jon Hopkins":"Jon Hopkins", "DJ Krush":"DJ Krush","DJ Shadow":"DJ Shadow", "Tupac":"Tupac", "Notorious":"Notorious"]
+        let artistGeneration = PopulationOfArrays();
         
+        picker1Options = artistGeneration.artistValues()
+        let firstValue = picker1Options[0]
+        picker2Options = artistGeneration.trackValues(firstValue)
+        
+        artistDictionary = ["ACDC": "ACDC", "BMTH": "BMTH", "DJ Krush":"DJ Krush","DJ Shadow":"DJ Shadow", "Tupac":"Tupac", "Notorious":"Notorious"]
+        trackDictionary = ["ACDC": "ACDC", "BMTH": "BMTH", "DJ Krush":"DJ Krush","DJ Shadow":"DJ Shadow", "Tupac":"Tupac", "Notorious":"Notorious"]
+
         let artistSortedKeys = Array(artistDictionary.keys).sort(<)
         artistArray = [artistSortedKeys,artistSortedKeys]
         
@@ -75,11 +116,6 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         for (var row=0;row<artistArray[0].count;row++){
             if (artistArray[0][row] == "ACDC"){
                 trackPicker.selectRow(row, inComponent: 0, animated: true)
-            }
-        }
-        for (var row=0;row<artistArray[1].count;row++){
-            if (artistArray[1][row] == "ACDC"){
-                trackPicker.selectRow(row, inComponent: 1, animated: true)
             }
         }
     }
@@ -93,28 +129,46 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
     // The number of rows of data
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
-        return artistDictionary.count
+        if (component == 0) {
+            return picker1Options.count
+        } else {
+            return picker2Options.count
+        }
+
     }
     
     // The data to return for the row and component (column) that's being passed in
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        
-        return artistArray[component][row]
+        if component == 0 {
+            //print( "\(picker1Options[row])")
+            return "\(picker1Options[row])"
+            
+        }
+        else {
+            //print( "\(picker2Options[row])")
+            return "\(picker2Options[row])"
+        }
     }
     
     // Capture the picker view selection
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
-        if(component == 0){
-            track = trackDictionary[trackArray[component][row]]
-            artist = artistDictionary[artistArray[component][row]]
+        if component == 0 {
+            let artistGeneration = PopulationOfArrays();
+            let currentValue = picker1Options[row]
+            picker2Options = artistGeneration.trackValues(currentValue)
+            pickerView.reloadAllComponents()
         }
-        else if (component == 1){
-            track = trackDictionary[trackArray[component][row]]
-        }
+
         
-        print(track)
+        else if(component == 1){
+            
+            track = "\(picker2Options[row])"
+            print(track)
+
+                }
+        
         
         let path = NSBundle.mainBundle().URLForResource(track, withExtension: "mp3")
         try! audioPlayer = AVAudioPlayer(contentsOfURL: path!, fileTypeHint: error)
@@ -124,8 +178,3 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         super.didReceiveMemoryWarning()
     }
 }
-
-
-
-
-
